dcd659b6db9d1e76a02aec8260411673363d305c
Steve Gardner <github@ste.vg>
2018-12-24 12:27:08 -0800

    cleared old files

--- beeptest.js
+++
@@ -1,5 +0,0 @@
-const Beep = require('./Beep').Beep;
-const beep = new Beep();
-
-setTimeout(() => {beep.successBeep()}, 3000);
-setTimeout(() => {beep.errorBeep()}, 5000);
--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 const Event = require('./Event').Event;
 const Listener = require('./Listener').Listener;
-//const Beep = require('./Beep').Beep;
 
 const listener = new Listener();
-//const beep = new Beep();
 
-let currentEvent = null;
+var currentEvent = null;
 
 listener.onEvent(event => 
 {
     if(event)
     {
-       // beep.successBeep();
         currentEvent = new Event(event);
     }
     else
     {
-      //  beep.errorBeep();
+
     }
 });
